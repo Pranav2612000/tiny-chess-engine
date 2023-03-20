22a3a900b7dfd74f5734f612c4dbbd2df62d20f1
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-20 10:16:10 +0530

    test: verify that piece.getValue() returns right values

--- piece_test.go
+++ piece_test.go
@@ -5,5 +5,33 @@ import (
 )
 
 func TestValue(t *testing.T) {
-    t.Fatalf(`Test failed`)
+    pawn := Piece{color: 'W', variant: 'P'}
+    if pawn.Value() != 100 {
+        t.Error(`Piece of type Pawn should have value`, pawn.Value());
+    }
+
+    knight := Piece{color: 'B', variant: 'N'}
+    if knight.Value() != 280 {
+        t.Error(`Piece of type Knight should have value`, pawn.Value());
+    }
+
+    bishop:= Piece{color: 'W', variant: 'B'}
+    if bishop.Value() != 320 {
+        t.Error(`Piece of type Bishop should have value`, pawn.Value());
+    }
+
+    rook := Piece{color: 'B', variant: 'R'}
+    if rook.Value() != 479 {
+        t.Error(`Piece of type Rook should have value`, pawn.Value());
+    }
+
+    queen := Piece{color: 'B', variant: 'Q'}
+    if queen.Value() != 929 {
+        t.Error(`Piece of type Queen should have value`, pawn.Value());
+    }
+
+    king := Piece{color: 'W', variant: 'K'}
+    if king.Value() != 99999 {
+        t.Error(`Piece of type White should have value`, pawn.Value());
+    }
 }
