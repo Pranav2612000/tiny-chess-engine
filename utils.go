d1b2b596dfabedf4624f73abaa5114a6b58d70f9
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-20 22:30:20 +0530

    feat: create a utils func to check if a string is uppercase

---
+++ utils.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "unicode"
+)
+
+func IsUpper(s string) bool {
+    for _, r := range s {
+        if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+            return false
+        }
+    }
+    return true
+}
