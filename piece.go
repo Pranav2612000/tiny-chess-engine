17cb02aa07fb567c168836888fd6f7a63435d5d1
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-22 10:36:48 +0530

    feat: create a map of moves for all pieces

--- piece.go
+++ piece.go
@@ -17,7 +17,17 @@ type Piece struct {
     variant byte 
 }
 
+const N, E, S, W = -10, 1, 10, 1;
+
 var pieceValueMap = map[byte]int{'.': 0, '-': 0, 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 99999 }
+var movesMap = map[byte][]int{
+    'P': {N, N + N, N + W, N + E},
+    'N': {N + N + E, N + N + W, E + E + N, E + E + S, S + S + E, S + S + W, W + W + S, W + W + N},
+    'B': {N + E, S + E, N + W, S + W},
+    'R': {N, S, E, W},
+    'Q': {N, S, E, W, N + E, S + E, N + W, S + W},
+    'K': {N, S, E, W, N + E, S + E, N + W, S + W},
+}
 
 func (p Piece) Value() int {
     return pieceValueMap[p.variant];
