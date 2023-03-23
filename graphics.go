d719d9811683d67f76efdd762c36f6d587609998
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-23 10:45:08 +0530

    ui: make board larger, and add pieces to board

--- graphics.go
+++ graphics.go
@@ -6,15 +6,22 @@ import (
 
 func DrawBoard(b *Board) {
     for i := 1; i <= 8; i = i + 1 {
-        fmt.Print("        —— —— —— —— —— —— —— ——\n");
+        fmt.Print("        ——— ——— ——— ——— ——— ——— ——— ———\n");
 
-        fmt.Printf("%d   %d ",10 * (10 - i), i);
-        for j := 0; j < 8; j = j + 1 {
-            fmt.Print("|  ");
+        fmt.Printf("%d   %d ",10 * (10 - i), (9 - i));
+        for j := 1; j <= 8; j = j + 1 {
+            var ch rune;
+
+            if b[10 * (10 - i) + j].piece == nil {
+                ch = ' ';
+            } else {
+                ch = rune(b[10 * (10 - i) + j].piece.variant);
+            }
+            fmt.Printf("| %c ", ch);
         }
         fmt.Print("|\n");
     }
-    fmt.Print("        —— —— —— —— —— —— —— ——\n");
-    fmt.Print("        a  b  c  d  e  f  g  h \n");
-    fmt.Print("        1  2  3  4  5  6  7  8\n");
+    fmt.Print("        ——— ——— ——— ——— ——— ——— ——— ———\n");
+    fmt.Print("         a   b   c   d   e   f   g   h \n");
+    fmt.Print("         1   2   3   4   5   6   7   8\n");
 }
