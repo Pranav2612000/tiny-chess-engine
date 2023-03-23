4e1f841434c0bd79f5e18db4648d684cfaaaa3cd
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-23 11:07:43 +0530

    ui: create func to draw board with possible moves

--- graphics.go
+++ graphics.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "golang.org/x/exp/slices"
 )
 
 func DrawBoard(b *Board) {
@@ -16,6 +17,31 @@ func DrawBoard(b *Board) {
                 ch = ' ';
             } else {
                 ch = rune(b[10 * (10 - i) + j].piece.variant);
+            }
+            fmt.Printf("| %c ", ch);
+        }
+        fmt.Print("|\n");
+    }
+    fmt.Print("        ——— ——— ——— ——— ——— ——— ——— ———\n");
+    fmt.Print("         a   b   c   d   e   f   g   h \n");
+    fmt.Print("         1   2   3   4   5   6   7   8\n");
+}
+
+func DrawBoardWithMoves(b *Board, moves []Square) {
+    for i := 1; i <= 8; i = i + 1 {
+        fmt.Print("        ——— ——— ——— ——— ——— ——— ——— ———\n");
+
+        fmt.Printf("%d   %d ",10 * (10 - i), (9 - i));
+        for j := 1; j <= 8; j = j + 1 {
+            var ch rune;
+            linearIndex := 10 * (10 - i) + j;
+
+            if slices.IndexFunc(moves, func(sq Square) bool { return sq.position == linearIndex }) != -1 {
+                ch = 'x';
+            } else if b[linearIndex].piece == nil {
+                ch = ' ';
+            } else {
+                ch = rune(b[linearIndex].piece.variant);
             }
             fmt.Printf("| %c ", ch);
         }
