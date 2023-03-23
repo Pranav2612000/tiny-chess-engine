eaa8e9f9e6a2f74df820ae6e3781a4c0cc498067
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-23 10:44:23 +0530

    fix: update FlipBoardTest to use new board logic

--- board_test.go
+++ board_test.go
@@ -9,32 +9,32 @@ func TestBoardFlip(t *testing.T) {
     // - white h2, e4, Nf3
     // - black e7, Ra8
     board := GenerateEmptyBoard();
-    board[33].piece = &Piece{color: 'W', variant: 'P'}
+    board[38].piece = &Piece{color: 'W', variant: 'P'}
     board[55].piece = &Piece{color: 'W', variant: 'P'}
-    board[44].piece = &Piece{color: 'W', variant: 'N'}
-    board[91].piece = &Piece{color: 'B', variant: 'P'}
-    board[99].piece = &Piece{color: 'B', variant: 'R'}
+    board[46].piece = &Piece{color: 'W', variant: 'N'}
+    board[85].piece = &Piece{color: 'B', variant: 'p'}
+    board[91].piece = &Piece{color: 'B', variant: 'r'}
 
     board.Flip();
 
-    if board[86].piece.variant != 'p' ||
-        board[86].position != 86 {
+    if board[81].piece.variant != 'p' ||
+        board[81].position != 81 {
         t.Error(`Board Flip failed for h2`);
     }
     if board[64].piece.variant != 'p' ||
         board[64].position != 64 {
         t.Error(`Board Flip failed for e4`);
     }
-    if board[75].piece.variant != 'n' ||
-        board[75].position != 75 {
+    if board[73].piece.variant != 'n' ||
+        board[73].position != 73 {
         t.Error(`Board Flip failed for Nf3`);
     }
-    if board[28].piece.variant != 'p' ||
-        board[28].position != 28 {
+    if board[34].piece.variant != 'P' ||
+        board[34].position != 34 {
         t.Error(`Board Flip failed for e7`);
     }
-    if board[20].piece.variant != 'r' ||
-        board[20].position != 20 {
+    if board[28].piece.variant != 'R' ||
+        board[28].position != 28 {
         t.Error(`Board Flip failed for Ra8`);
     }
 }
