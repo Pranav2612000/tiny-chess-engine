26a1468b4643d1eb13e51bdae85d91513c301607
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-24 23:44:49 +0530

    feat: create func to return starting position board

--- board.go
+++ board.go
@@ -32,3 +32,49 @@ func GenerateEmptyBoard() Board {
     }
     return board;
 }
+
+func GenerateInitialPositionBoard() Board {
+    board := GenerateEmptyBoard();
+
+    // White pieces
+    board[21].piece =  &Piece{color: 'W', variant: 'R'}
+    board[22].piece =  &Piece{color: 'W', variant: 'N'}
+    board[23].piece =  &Piece{color: 'W', variant: 'B'}
+    board[24].piece =  &Piece{color: 'W', variant: 'Q'}
+    board[25].piece =  &Piece{color: 'W', variant: 'K'}
+    board[26].piece =  &Piece{color: 'W', variant: 'B'}
+    board[27].piece =  &Piece{color: 'W', variant: 'N'}
+    board[28].piece =  &Piece{color: 'W', variant: 'R'}
+
+    // White pawns
+    board[31].piece =  &Piece{color: 'W', variant: 'P'}
+    board[32].piece =  &Piece{color: 'W', variant: 'P'}
+    board[33].piece =  &Piece{color: 'W', variant: 'P'}
+    board[34].piece =  &Piece{color: 'W', variant: 'P'}
+    board[35].piece =  &Piece{color: 'W', variant: 'P'}
+    board[36].piece =  &Piece{color: 'W', variant: 'P'}
+    board[37].piece =  &Piece{color: 'W', variant: 'P'}
+    board[38].piece =  &Piece{color: 'W', variant: 'P'}
+
+    // Black pieces
+    board[91].piece =  &Piece{color: 'B', variant: 'r'}
+    board[92].piece =  &Piece{color: 'B', variant: 'n'}
+    board[93].piece =  &Piece{color: 'B', variant: 'b'}
+    board[94].piece =  &Piece{color: 'B', variant: 'q'}
+    board[95].piece =  &Piece{color: 'B', variant: 'k'}
+    board[96].piece =  &Piece{color: 'B', variant: 'b'}
+    board[97].piece =  &Piece{color: 'B', variant: 'n'}
+    board[98].piece =  &Piece{color: 'B', variant: 'r'}
+
+    // Black pawns
+    board[81].piece =  &Piece{color: 'B', variant: 'p'}
+    board[82].piece =  &Piece{color: 'B', variant: 'p'}
+    board[83].piece =  &Piece{color: 'B', variant: 'p'}
+    board[84].piece =  &Piece{color: 'B', variant: 'p'}
+    board[85].piece =  &Piece{color: 'B', variant: 'p'}
+    board[86].piece =  &Piece{color: 'B', variant: 'p'}
+    board[87].piece =  &Piece{color: 'B', variant: 'p'}
+    board[88].piece =  &Piece{color: 'B', variant: 'p'}
+
+    return board;
+}
