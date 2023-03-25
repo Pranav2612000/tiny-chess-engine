a5e563fc23b7582f0bb224fbea4385a90096b3cc
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-25 19:58:48 +0530

    feat: allow 2 players to start a game and make moves

--- main.go
+++ main.go
@@ -5,5 +5,37 @@ import (
 )
 
 func main() {
-    fmt.Println("Starting Chess Engine...")
+    fmt.Println("Starting a new game")
+
+    board := GenerateInitialPositionBoard();
+    position := Position{
+        board: &board,
+        score: 0,
+        wc: [2]bool{false, false},
+        bc:[2]bool{false, false},
+        ep: nil,
+        kp: nil,
+        turn: true,
+    };
+    DrawBoard(position.board);
+    var move string;
+    for true {
+        fmt.Scanln(&move);
+
+        if move == "exit" {
+            break;
+        }
+
+        err := position.MoveFromNotation(move);
+
+        if err != nil {
+            fmt.Printf("Error: %v\n", err);
+            fmt.Println("Please enter a valid move");
+            continue;
+        }
+
+        position.Flip();
+        DrawBoard(position.board);
+    }
+    fmt.Println("Thank you for playing");
 }
