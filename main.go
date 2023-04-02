1ef59e5b099bd89c7bb94ed13efa640d859ef8f6
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-02 11:40:24 +0530

    feat: allow choosing between 2 player and single player game

--- main.go
+++ main.go
@@ -2,58 +2,73 @@ package main
 
 import (
     "fmt"
+    "flag"
 )
 
+func startTwoPlayerGame () {
+  fmt.Println("Starting a new 2 player game")
+  board := GenerateInitialPositionBoard();
+  position := Position{
+      board: &board,
+      score: 0,
+      wc: [2]bool{false, false},
+      bc:[2]bool{false, false},
+      ep: nil,
+      kp: nil,
+      turn: true,
+  };
+  DrawBoard(position.board);
+  var move string;
+  for true {
+      var color string;
+      var oppositeColor string;
+      if position.turn {
+          color = "white";
+          oppositeColor = "black"
+      } else {
+          color = "black";
+          oppositeColor = "white"
+      }
+
+      // If the current player has no possible moves we end the game
+      moves := position.Moves();
+      if moves.GetNumberOfMoves() == 0 {
+          fmt.Printf("Game over!! %s wins\n", oppositeColor);
+          break;
+      }
+
+      fmt.Printf("%s's move: ", color);
+      fmt.Scanln(&move);
+
+      if move == "exit" {
+          break;
+      }
+
+      err := position.MoveFromNotation(move);
+
+      if err != nil {
+          fmt.Printf("Error: %v\n", err);
+          fmt.Println("Please enter a valid move");
+          continue;
+      }
+      position.turn = !position.turn;
+
+      DrawBoard(position.board);
+  }
+  fmt.Println("Thank you for playing");
+}
+
+func startGameWithComputer() {
+  fmt.Println("Starting a new game with Computer")
+}
+
 func main() {
-    fmt.Println("Starting a new game")
+  isTwoPlayerGame := flag.Bool("two-player", false, "Starts a two player game");
+  flag.Parse();
 
-    board := GenerateInitialPositionBoard();
-    position := Position{
-        board: &board,
-        score: 0,
-        wc: [2]bool{false, false},
-        bc:[2]bool{false, false},
-        ep: nil,
-        kp: nil,
-        turn: true,
-    };
-    DrawBoard(position.board);
-    var move string;
-    for true {
-        var color string;
-        var oppositeColor string;
-        if position.turn {
-            color = "white";
-            oppositeColor = "black"
-        } else {
-            color = "black";
-            oppositeColor = "white"
-        }
-
-        // If the current player has no possible moves we end the game
-        moves := position.Moves();
-        if moves.GetNumberOfMoves() == 0 {
-            fmt.Printf("Game over!! %s wins\n", oppositeColor);
-            break;
-        }
-
-        fmt.Printf("%s's move: ", color);
-        fmt.Scanln(&move);
-
-        if move == "exit" {
-            break;
-        }
-
-        err := position.MoveFromNotation(move);
-
-        if err != nil {
-            fmt.Printf("Error: %v\n", err);
-            fmt.Println("Please enter a valid move");
-            continue;
-        }
-        position.turn = !position.turn;
-
-        DrawBoard(position.board);
-    }
-    fmt.Println("Thank you for playing");
+  if (*isTwoPlayerGame) {
+    startTwoPlayerGame();
+  } else {
+    startGameWithComputer();
+  }
 }
