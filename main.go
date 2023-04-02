b1ae012a27a6583f549bc2c3dd6090c358a9c8ee
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-02 14:01:46 +0530

    feat: basic algorithm used for finding the best move

--- main.go
+++ main.go
@@ -59,7 +59,66 @@ func startTwoPlayerGame () {
 }
 
 func startGameWithComputer() {
+  computerColor := "white";
+  s := &Searcher{ tp: map[Position]entry{} }
+
   fmt.Println("Starting a new game with Computer")
+  board := GenerateInitialPositionBoard();
+  position := Position{
+      board: &board,
+      score: 0,
+      wc: [2]bool{false, false},
+      bc:[2]bool{false, false},
+      ep: nil,
+      kp: nil,
+      turn: true,
+  };
+
+  var move string;
+  for true {
+    var color string;
+    var oppositeColor string;
+    if position.turn {
+      color = "white";
+      oppositeColor = "black";
+    } else {
+      color = "black";
+      oppositeColor = "white";
+    }
+
+    // If the current player has no possible moves we end the game
+    moves := position.Moves();
+    if moves.GetNumberOfMoves() == 0 {
+        fmt.Printf("Game over!! %s wins\n", oppositeColor);
+        break;
+    }
+
+    if color == computerColor {
+      moveRaw := s.SearchMove(position.Copy(), 10000);
+      position.Move(moveRaw);
+      DrawBoard(position.board);
+    } else {
+      //fmt.Printf("The computer played %v\n", move);
+      fmt.Printf("Your move: ");
+      fmt.Scanln(&move);
+
+      if move == "exit" {
+          break;
+      }
+
+      err := position.MoveFromNotation(move);
+
+      if err != nil {
+          fmt.Printf("Error: %v\n", err);
+          fmt.Println("Please enter a valid move");
+          continue;
+      }
+    }
+
+    position.turn = !position.turn;
+
+  }
+  fmt.Println("Thank you for playing");
 }
 
 func main() {
---
+++ searcher.go
@@ -0,0 +1,83 @@
+package main
+
+var (
+  MateValue = pieceValueMap['K'] + 10 * pieceValueMap['Q'] // A very large value to denote mate score
+  MaxTableSize = 10000000 // length of the transposition table
+)
+
+type entry struct {
+  depth int // depth of the search
+  score int // score at the current move
+
+  // Values used for alpha beta pruning -
+  alpha float64 // alpha value at the current node
+  beta float64 // beta value at the current node
+
+  // Value used for windowing
+  gamma int
+
+  // best move at this node
+  move Move
+}
+
+type Searcher struct {
+  tp map[Position]entry // transposition table to keep track of best move at a position
+  nodes int // Number of nodes to consider, to keep a check on the complexity of the calculations
+}
+
+func (s *Searcher) SearchMove(pos Position, maxNodes int) (m Move) {
+  s.nodes = 0;
+
+  for depth := 1; depth < 2; depth++ {
+    alpha, beta := float64(-3 * MateValue), float64(3 * MateValue);
+    gamma := int(( alpha + beta + 1 ) / 2);
+    s.Search(pos, alpha, beta, gamma, depth);
+  }
+
+  return s.tp[pos].move;
+}
+
+func (s *Searcher) Search(pos Position, alpha float64, beta float64, gamma int, depth int) (score int) {
+  s.nodes++;
+
+  if Abs(pos.score) > MateValue {
+    return pos.score;
+  }
+
+  bestScore, bestMove := -3*MateValue, Move{};
+
+  allMoves := pos.Moves()
+  for start, moves := range allMoves {
+    for _, sq := range moves {
+      
+      if depth <= 0 {
+        break;
+      }
+
+      startCp := start.Copy();
+      sqCp := sq.Copy();
+      move := Move{ from: &startCp, to: &sqCp} 
+
+      flippedPos := pos.Copy();
+      flippedPos.Move(move);
+      flippedPos.Flip();
+
+      score := -1 * s.Search(flippedPos, beta, alpha, gamma, depth - 1);
+
+      if score > bestScore {
+        bestScore, bestMove = score, move
+      }
+    }
+  }
+
+  s.tp[pos] = entry{
+    depth: depth,
+    score: bestScore,
+    alpha: alpha,
+    beta: beta,
+    gamma: gamma,
+    move: bestMove,
+  };
+
+  return bestScore;
+}
